4b96e71adcf783110f0b2ffbaa86f8f763bd9e61
boydenkoad <anton.boydenko.dev@gmail.com>
2024-01-28 14:46:30 +0700

    second commit

--- java-script/index.js
+++ java-script/index.js
@@ -1,1 +1,11 @@
 console.log('Hello world')
+
+const title = document.querySelector('#title')
+
+title.addEventListener('click',(call)=>{
+	if(title.classList.contains('red')){
+		title.classList.remove('red')
+	}else{
+		title.classList.add('red')
+	}
+})
